55d19c3bf2d33adf2e0bf6e61c351cfd2fcd4336
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-20 16:08:37 +0200

    Il programma calcola il prezzo della corsa totale in base ai chilometri

--- js/script.js
+++ js/script.js
@@ -9,4 +9,12 @@ document.getElementById("km-todo").innerHTML = `La distanza da percorrere sarà 
 const customerAge = Number(prompt("Inserisci la tua età:"));
 document.getElementById("customer-age").innerHTML = `Età del passeggero: ${customerAge} anni.`;
 
+/*Sulla base di queste informazioni dovrà calcolare il 
+prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+L'output del prezzo finale va messo fuori in forma umana 
+(con massimo due decimali, per indicare centesimi sul prezzo).*/
+const ticketPrice = ((0.21 * kmDistance));
+document.getElementById("ticket-price").innerHTML =`Il prezzo totale della corsa è di ${ticketPrice.toFixed(2)} Euro.`;
+
 
