59dec91f00230ab65e48583a0f8e7aee8fceea40
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-20 16:08:04 +0200

    Browser chiede adesso anche l'età dell'utente

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ che vuole percorrere*/
 const kmDistance = Number(prompt("Quanti kilometri dovrai percorrere?"));
 document.getElementById("km-todo").innerHTML = `La distanza da percorrere sarà di ${kmDistance} km.`;
 
+/*e l'età del passeggero.*/
+const customerAge = Number(prompt("Inserisci la tua età:"));
+document.getElementById("customer-age").innerHTML = `Età del passeggero: ${customerAge} anni.`;
+
 
