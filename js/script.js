68a9acd5299cb25a776c5b0484a8e9e32c7b5e5a
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-20 16:09:46 +0200

    Creato un blocco di if che controlla le varie età inserite dall'utente: - se viene inserita un'età inferiore a 1 o maggiore di 110 il programma si arresta e si aggiorna la finestra - if se l'età è inferiore a 18 - if se l'età è maggiore o uguale a 65

--- js/script.js
+++ js/script.js
@@ -17,4 +17,18 @@ L'output del prezzo finale va messo fuori in forma umana
 const ticketPrice = ((0.21 * kmDistance));
 document.getElementById("ticket-price").innerHTML =`Il prezzo totale della corsa è di ${ticketPrice.toFixed(2)} Euro.`;
 
-
+/*va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.*/
+let discountPrice;
+if(customerAge < 1 || customerAge > 110){
+    alert("Assicurati di inserire un'età compresa tra 1 e 110.");
+    location.reload();
+}else if (customerAge < 18){
+    document.getElementById("age-discount").innerHTML = "Congratulazioni! Hai diritto ad uno sconto del 20% sul totale della corsa!";
+    discountPrice = (ticketPrice) - (ticketPrice * 0.2);
+    document.getElementById("new-discounted-price").innerHTML = `Il prezzo totale della corsa è di ${discountPrice.toFixed(2)} Euro.`;
+} else if (customerAge >= 65){
+    document.getElementById("age-discount").innerHTML = "Congratulazioni! Lei ha diritto ad uno sconto del 40% sul totale della corsa!";
+    discountPrice = (ticketPrice) - (ticketPrice * 0.4);
+    document.getElementById("new-discounted-price").innerHTML = `Il prezzo totale della corsa è di ${discountPrice.toFixed(2)} Euro.`;
+}
